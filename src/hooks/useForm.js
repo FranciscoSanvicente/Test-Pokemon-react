abbf2de804b891d93359d12e26d74eac53452ed5
Francisco Sanvicente Pico <64644109+FranciscoSanvicente@users.noreply.github.com>
2021-08-31 21:28:57 -0400

    Update useForm.js

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,24 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
+export const useForm = (initialState = {}) => {
+  const [values, setValues] = useState(initialState);
 
-export const useForm = ( initialState = {} ) => {
-    
-    const [values, setValues] = useState(initialState);
+  const reset = () => {
+    setValues(initialState);
+  };
 
-    const reset = () => {
-        setValues( initialState );
-    }
+  const handleInputChange = ({ target }) => {
+    setValues({
+      ...values,
+      [target.name]: target.value,
+    });
+  };
 
-
-    const handleInputChange = ({ target }) => {
-
-        setValues({
-            ...values,
-            [ target.name ]: target.value
-        });
-
-    }
-
-    return {values, handleInputChange, reset} ;
-
-}
+  return { values, handleInputChange, reset, setValues };
+};
